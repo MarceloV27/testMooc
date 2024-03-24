84d0bf095afd00a27e9a9e7c12accf94446d407a
MarceloV27 <marceloveintimilla3@gmail.com>
2024-03-24 19:27:52 -0400

    For loop exercise update

--- javaForLoop/src/Question3/Main.java
+++ javaForLoop/src/Question3/Main.java
@@ -1,6 +1,33 @@
 package Question3;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number for display multiplication table");
+        int numberMutil = scanner.nextInt();
+        int result = 0;
+
+        while(true) {
+
+            if(numberMutil == 0){
+                break;
+            }
+
+            for (int i = 1; i <= 10; i++) {
+
+                result = numberMutil * i;
+
+                System.out.println(result);
+            }
 
 
+        }
+
+        System.out.println("Exit the program");
+
+    }
 }
