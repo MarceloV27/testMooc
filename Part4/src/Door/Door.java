d06e8e11fa0d6a953fd6f5fe1d6191543411c3be
Marcelo Desktop <marceloveintimilla3@gmail.com>
2024-04-24 11:19:17 -0400

    Update Mooc Part 4 Introduction to object-Oriented programming

---
+++ Part4/src/Door/Door.java
@@ -0,0 +1,13 @@
+package Door;
+
+public class Door {
+
+    public Door(){
+    }
+
+    public void knock(){
+        System.out.println("Who's there?");
+    }
+
+
+}
---
+++ Part4/src/deceasingCounter/Product.java
@@ -0,0 +1,21 @@
+package deceasingCounter;
+
+public class Product {
+
+
+    private final String initialName;
+    private final double initialPrice;
+    private final int initialQuantity;
+
+    public Product(String initialName, double initialPrice, int initialQuantity){
+        this.initialName = initialName;
+        this.initialPrice = initialPrice;
+        this.initialQuantity = initialQuantity;
+    }
+
+    public void printProduct(){
+        System.out.println(initialName+ "," + initialPrice + "," + initialQuantity);
+
+    }
+
+}
