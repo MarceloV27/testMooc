299cd40382b89359cf70ced2d038f0732475c4a7
Marcelo Desktop <marceloveintimilla3@gmail.com>
2024-05-08 11:15:16 -0400

    Update part4 of the Java Mooc (Introduction to object-oriented programming.

--- Part4/src/PaymentCard/Main.java
+++ Part4/src/PaymentCard/Main.java
@@ -2,10 +2,62 @@ package PaymentCard;
 
 public class Main {
 
+
     public static void main(String[] args) {
 
+       /*   Part 1:
         PaymentCard card = new PaymentCard(50);
+        System.out.println(card);*/
+
+    /*
+      Part 2:
+     PaymentCard card = new PaymentCard(50);
         System.out.println(card);
+
+        card.eatAffordably();
+        System.out.println(card);
+
+        card.eatHeartily();
+        card.eatAffordably();
+        System.out.println(card);
+*/
+
+      /*  Part 3:
+     PaymentCard card = new PaymentCard(6);
+        System.out.println(card);
+
+        card.eatAffordably();
+        System.out.println(card);
+
+        card.eatHeartily();
+        card.eatAffordably();
+        System.out.println(card);*/
+
+
+
+        PaymentCard card = new PaymentCard(-1);
+        System.out.println(card);
+
+        card.addMoney(15);
+        System.out.println(card);
+
+        card.addMoney(10);
+        System.out.println(card);
+
+        card.addMoney(200);
+        System.out.println(card);
+
+
+
+       /* PaymentCard card = new PaymentCard(100);
+        System.out.println("Paul: " + card);
+        card.addMoney(-15);
+        System.out.println("Paul: " + card);
+
+*/
+
+
+
 
     }
 }
--- Part4/src/PaymentCard/PaymentCard.java
+++ Part4/src/PaymentCard/PaymentCard.java
@@ -10,21 +10,48 @@ public class PaymentCard {
     }
 
     public void eatAffordably(){
+        // In side to this method you need to subtract $2.60 to your BALANCE
 
+        if(balance > 2.6){
+            balance = balance - 2.6;
+        }else if(balance ==0){
+            balance = 0;
+        }else{
+            balance *= 1;
+        }
     }
 
     public void eatHeartily(){
+        // In side to this method you need to subtract $4.60 to your BALANCE
 
+        if(balance > 4.60){
+            balance = balance - 4.60;
+        }else if (balance == 0){
+            balance = 0;
+        }else{
+            balance *= 1;
+        }
     }
 
-    public void addMoney(double deposit){
+    public void addMoney(double deposit) {
+        // In this method variable DEPOSIT increase until $150 and do not display negative.
+
+
+       if (deposit < 0) {
+            balance *= 1;
+        } else if (deposit > 150) {
+            balance = 150;
+        } else if (balance <= 150) {
+            balance += deposit;
+
+        }
 
     }
 
 
     public String toString(){
-
-        return "The card has a balance of " + this.balance +" euros";
+        // NOT CHANGE IT BECAUSE IS A STRING!
+        return "The card has a balance of " + balance +" euros";
     }
 
 
