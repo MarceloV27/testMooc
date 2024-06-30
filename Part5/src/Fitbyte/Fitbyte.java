087aeaa1b51f1e331eacc0a7220b2575e3df80c4
Marcelo Desktop <marceloveintimilla3@gmail.com>
2024-06-30 09:54:22 -0400

    Finish the last exercise from part 5.1

--- Part5/src/Fitbyte/Fitbyte.java
+++ Part5/src/Fitbyte/Fitbyte.java
@@ -1,5 +1,7 @@
 package Fitbyte;
 
+import java.util.ArrayList;
+
 public class Fitbyte {
 
     private int age;
@@ -12,9 +14,9 @@ public class Fitbyte {
 
     public double targetHeartRate(double percentageOfMaximum){
 
-        double resultMax = 200 - (percentageOfMaximum * age);
-        return (resultMax - restingHeartRate)*(percentageOfMaximum)+ restingHeartRate;
-    }
+        double resultMax = 206.3 - (0.711 * age);
+        return  ((resultMax - restingHeartRate)*(percentageOfMaximum) + restingHeartRate);
 
+    }
 
 }
